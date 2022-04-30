f372fbf6c92c36e779186c77092fadbe33395bfc
jax2net <jacob@jax2.net>
2022-04-30 15:13:21 -0700

    JSON obj parsing

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,22 @@
 const Phaser = require('phaser');
+const socket = new WebSocket('ws://localhost:5001');
+
+const obj = {
+    action: 'movement',
+    x: 50,
+    y: 25
+};
+
+socket.addEventListener('open', function(event)
+{
+    socket.send(JSON.stringify(obj));
+});
+
+socket.addEventListener('message', function(event)
+{
+    console.log(`origin: ${event.origin}`);
+    console.log(`data: ${event.data}`);
+});
 
 const cfg = {
     type: Phaser.AUTO,
@@ -21,15 +39,67 @@ const cfg = {
 };
 
 const game = new Phaser.Game(cfg);
+let player: any;
+let platforms: any;
+let cursors: any;
 
-function preload()
+function preload(this: any)
 {
+    this.load.image('ground', '../assets/platform.png');
+    this.load.spritesheet('dude', '../assets/dude.png', {
+        frameWidth: 32,
+        frameHeight: 48
+    });
 }
 
-function create()
+function create(this: any)
 {
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setCollideWorldBounds(true);
+
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.physics.add.collider(player, platforms);
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update()
 {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    }
+    else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const webpack = require('webpack');
 const path = require('path');
+const fs = require('fs');
 
 module.exports = {
     entry: './src/game.js',
     mode: 'development',
+    target: 'web',
     output: {
         filename: 'app.build.js',
         path: path.resolve(__dirname, 'build'),
